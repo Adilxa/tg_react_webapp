380695553146ea4d71645faa94c866a653cd7c35
Adilxa <adilhanimerov82@gmail.com>
2024-06-03 14:54:52 +0600

    feat jello

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
+  const app = window.Telegram?.WebApp;
   const [appData, setAppData] = useState(null);
-  const [chatId, setChatId] = useState(null);
 
   useEffect(() => {
     const fetchAppData = async () => {
       if (window.Telegram && window.Telegram.WebApp) {
         const app = window.Telegram.WebApp;
-        app.ready(); // Make sure the app is ready before accessing initDataUnsafe
+        await app.ready(); // Убедитесь, что app готов перед обращением к initDataUnsafe
         setAppData(app.initDataUnsafe);
-
-        // Extract chat_id from initData
-        const params = new URLSearchParams(app.initData);
-        const chatId = params.get("chat_id");
-        setChatId(chatId);
       }
     };
     fetchAppData();
   }, []);
-
   return (
     <div className="App">
       <header className="App-header">
@@ -34,12 +28,10 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ></a>
         <div>{appData?.user?.first_name}</div>
         <div>
-          <h1>{chatId} da</h1>
+          <h1>{appData?.chat?.id}</h1>
         </div>
       </header>
     </div>
