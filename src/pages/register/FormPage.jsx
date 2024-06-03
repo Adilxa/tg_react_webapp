29156b1ff089bfaba97490b1692b7f296a348fa4
Adilxa <adilhanimerov82@gmail.com>
2024-06-03 16:27:19 +0600

    feat: valifdation

--- src/pages/register/FormPage.jsx
+++ src/pages/register/FormPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTelegramHook } from '../../hooks/useTelegramHook';
 import './FormPage.css';
 import axios from 'axios';
@@ -10,6 +10,7 @@ function FormPage() {
     const [inn, setInn] = useState("");
     const [password, setPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState(null);
+    const [isChatExist, setChatExist] = useState(false)
 
     const onSendRequest = async (e) => {
         e.preventDefault();
@@ -44,20 +45,46 @@ function FormPage() {
     };
 
 
-    return (
-        <div className="container">
-            {user?.id}
-            <div className="card">
-                <h2>Enter Inn & Password</h2>
-                <form onSubmit={onSendRequest}>
-                    <input onChange={(e) => setInn(e.target.value)} value={inn} type="text" placeholder="Inn" />
-                    <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
-                    <button type="submit">Submit</button>
-                </form>
+    const ifChatExist = async () => {
+        try {
+            const res = await $api.get("registration" + user.id)
+            if (res.status === 200) {
+                setChatExist(true)
+            }
+        } catch (e) {
+            if (e.response.status === 404) {
+                setChatExist(false)
+            }
+        }
+
+    }
+
+    useEffect(() => {
+        ifChatExist()
+    }, [])
+
+    if (!isChatExist)
+        return (
+            <div className="container">
+                <div className="card">
+                    <h2>Enter Inn & Password</h2>
+                    <form onSubmit={onSendRequest}>
+                        <input onChange={(e) => setInn(e.target.value)} value={inn} type="text" placeholder="Inn" />
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+                {errorMessage && <Popup message={errorMessage} onClose={closePopup} />}
             </div>
-            {errorMessage && <Popup message={errorMessage} onClose={closePopup} />}
-        </div>
-    );
+        )
+    else {
+        return (
+            <section>
+                <h1>You are autherized!</h1>
+                <button onClick={onClose}>go to telegram</button>
+            </section>
+        )
+    }
 }
 
 export default FormPage;
